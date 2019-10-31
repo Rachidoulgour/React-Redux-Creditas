d1e6d3b1f2295781240e73575f357f0edf9f9a04
rachid <olgor@hotmail.com>
2019-10-31 15:41:09 +0100

    Signup and cart components started

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from "../src/views/Home";
 import Login from "../src/views/user/Login";
 import Header from '../src/components/Header';
 import './App.css';
+import SignUp from './views/user/SignUp';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <Switch>
       <Route path='/' component={Home} exact /> 
       <Route path='/login' component={Login} exact />
+      <Route path='/SignUp' component={SignUp} exact />
       </Switch>
       </div>
     </BrowserRouter>
